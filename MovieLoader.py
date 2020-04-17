58abb6e0e4022e0aafc076a9be0a828f1e4ee222
Dean Su <183184520@qq.com>
2020-04-17 16:32:18 -0700

    Update MovieLoader.py
    Changed variable names

--- MovieLoader.py
+++ MovieLoader.py
@@ -7,61 +7,61 @@ from surprise import Reader
 
 class MovieLoader:
 
-    movieID_to_name = {}
-    name_to_movieID = {}
-    path_to_ratings = './ml-latest-small/ratings.csv'
-    path_to_movies = './ml-latest-small/movies.csv'
+    filmIDtoName = {}
+    nameToFilmID = {}
+    pathOfRatings = './ml-latest-small/ratings.csv'
+    pathOfFilms = './ml-latest-small/movies.csv'
     
     def mLDataLoad(self):
 
         # Look for files relative to the directory we are running from
         os.chdir(os.path.dirname(sys.argv[0]))
 
-        ratingsDataset = 0
-        self.movieID_to_name = {}
-        self.name_to_movieID = {}
+        ratingsDS = 0
+        self.filmIDtoName = {}
+        self.nameToFilmID = {}
 
         reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
 
-        ratingsDataset = Dataset.load_from_file(self.path_to_ratings, reader=reader)
+        ratingsDS = Dataset.load_from_file(self.pathOfRatings, reader=reader)
 
-        with open(self.path_to_movies, newline='', encoding='ISO-8859-1') as csvfile:
-                movieReader = csv.reader(csvfile)
-                next(movieReader)  #Skip header line
-                for row in movieReader:
-                    movieID = int(row[0])
+        with open(self.pathOfFilms, newOfLine='', encoding='ISO-8859-1') as sourceFile:
+                filmReader = csv.reader(sourceFile)
+                next(filmReader)  #Skip header line
+                for row in filmReader:
+                    filmID = int(row[0])
                     movieName = row[1]
-                    self.movieID_to_name[movieID] = movieName
-                    self.name_to_movieID[movieName] = movieID
+                    self.filmIDtoName[filmID] = movieName
+                    self.nameToFilmID[movieName] = filmID
 
-        return ratingsDataset
+        return ratingsDS
 
-    def getUserRatings(self, user):
-        userRatings = []
-        hitUser = False
-        with open(self.path_to_ratings, newline='') as csvfile:
-            ratingReader = csv.reader(csvfile)
+    def loadClientRatings(self, client):
+        clientRatings = []
+        hitClient = False
+        with open(self.pathOfRatings, newOfLine='') as sourceFile:
+            ratingReader = csv.reader(sourceFile)
             next(ratingReader)
             for row in ratingReader:
-                userID = int(row[0])
-                if (user == userID):
-                    movieID = int(row[1])
+                clientID = int(row[0])
+                if (client == clientID):
+                    filmID = int(row[1])
                     rating = float(row[2])
-                    userRatings.append((movieID, rating))
-                    hitUser = True
-                if (hitUser and (user != userID)):
+                    clientRatings.append((filmID, rating))
+                    hitClient = True
+                if (hitClient and (client != clientID)):
                     break
 
-        return userRatings
+        return clientRatings
     
-    def getMovieName(self, movieID):
-        if movieID in self.movieID_to_name:
-            return self.movieID_to_name[movieID]
+    def getNameOfFilm(self, filmID):
+        if filmID in self.filmIDtoName:
+            return self.filmIDtoName[filmID]
         else:
             return ""
         
-    def getMovieID(self, movieName):
-        if movieName in self.name_to_movieID:
-            return self.name_to_movieID[movieName]
+    def getIDOfFilm(self, filmName):
+        if filmName in self.nameToFilmID:
+            return self.nameToFilmID[filmName]
         else:
             return 0
